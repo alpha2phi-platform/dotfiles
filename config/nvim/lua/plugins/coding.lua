7d129939154cf99d37dc59d87c0880393c636472
mengwangk <mengwangk@gmail.com>
2024-02-04 12:40:50 +0800

    fix: surround

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -142,21 +142,7 @@ return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
-    opts = {
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "gs",
-        normal_cur = "gss",
-        normal_line = "gS",
-        normal_cur_line = "gSS",
-        visual = "Z",
-        visual_line = "gz",
-        delete = "ds",
-        change = "cs",
-        change_line = "cS",
-      },
-    },
+    opts = {},
   },
   {
     "echasnovski/mini.surround",
